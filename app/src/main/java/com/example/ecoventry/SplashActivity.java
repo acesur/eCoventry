d0e59642af0a037c84d3a3e5e3b2881fc0bfcd26
d3rtb0y <sures.chaudhary0@gmail.com>
2019-12-12 23:32:19 +0545

    Splash Activity For the aplication

---
+++ app/src/main/java/com/example/ecoventry/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.ecoventry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Using a thread and halt screen for 2 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();// close the activity
+            }
+        },2000);
+    }
+}
